b77d556c80bd8f5255065693a9c8777fbd19a553
Malgorzata Kapuscinska <m_kapuscinska1@o2.pl>
2018-03-01 08:47:07 +0100

    Extract components - ready

--- js/scripts.js
+++ js/scripts.js
@@ -27,60 +27,16 @@ var movies = [
 		kindOf: 'Phantasy, Action',
 		desc: 'The first supernatural Ghost Rider is stunt motorcyclist Johnny Blaze, who, in order to save the life of his father, agreed to give his soul to "Satan" (later revealed to be an arch-demon named Mephisto). At night and when around evil, Blaze finds his flesh consumed by hellfire, causing his head to become a flaming skull. He rides a fiery motorcycle and wields blasts of hellfire from his body, usually from his skeletal hands.',
 		imgSrc: 'images/3.jpg',
-		video: 'https://youtu.be/8M0lLJfNPds'
+		videoSrc: 'https://youtu.be/8M0lLJfNPds'
 	}
 	
 ];
-
-var Movie = React.createClass({
-	propTypes: {
-		movie: React.PropTypes.object.isRequired,
-	},
-	render: function(){
-		return (
-				React.createElement('div', {key: this.props.movie.id, className: 'card'},	
-					React.createElement(CardLeft, {image: this.props.movie.imgSrc}),
-					React.createElement(CardRight, {}, )
-				)
-		)
-	}
-});
-		
-var CardLeft = React.createClass({
-	propTypes: {
-		imgSrc: React.propTypes.string.isRequired,
-	},
-	render: function(){
-		return (
-			React.createElement('div', {className: 'card_left'},
-				React.createElement('img', {src: this.props.movie.imgSrc})
-			)
-		)
-	}
-});
-
-
-var CardRight = React.createClass({
-	propTypes: {
-		movie: React.propTypes.object.isRequired,
-	},
-	render: function(){
-		return (
-				React.createElement('div', {className: 'card_right'},
-					React.createElement(MovieTitle, {}, this.props.movie.title),
-					React.createElement(MovieDetails, {}, this.props.movie),
-					React.createElement(MovieDescription, {}, this.props.movie.desc),
-					React.createElement(MovieButton, {}, this.props.movie.video),
-				)
-		)
-	}
-});
 
 // TITLE
 
 var MovieTitle = React.createClass({
 	propTypes: {
-		title: React.propTypes.string.isRequired, 
+		title: React.PropTypes.string.isRequired 
 	},
 	render: function() {
 		return (React.createElement('h2', {}, this.props.title)
@@ -92,14 +48,18 @@ var MovieTitle = React.createClass({
 
 var MovieDetails = React.createClass({
 	propTypes: {
-		movie: React.propTypes.object.isRequired,
+		movie: React.PropTypes.object.isRequired,
 	},
 	render: function(){
-		return Reaact.createElement('ul', {}, 
-				React.createElement('li', {}, this.props.movie.prodDate),
-				React.createElement('li', {}, this.props.movie.length),
-				React.createElement('li', {}, this.props.movie.kindOf)	
-		)
+		return (
+			React.createElement('div', {className: 'card_right_details'}, 
+				React.createElement('ul', {}, 
+					React.createElement('li', {}, this.props.movie.prodDate),
+					React.createElement('li', {}, this.props.movie.length),
+					React.createElement('li', {}, this.props.movie.kindOf)	
+				)
+			)
+		)	
 	}
 });
 
@@ -107,12 +67,12 @@ var MovieDetails = React.createClass({
 
 var MovieDescription = React.createClass({
 	propTypes: {
-		desc: Reaact.propTypes.string.isRequired,
+		desc: React.PropTypes.string.isRequired,
 	},
 	render: function(){
 		return (
 			React.createElement('div', {className: 'card_right_review'},
-					React.createElement('p', {}, this.props.movie.desc)
+					React.createElement('p', {}, this.props.desc)
 			)
 		)
 	}
@@ -122,25 +82,74 @@ var MovieDescription = React.createClass({
 
 var MovieButton = React.createClass({
 	propTypes: {
-		video: React.propTypes.string.idRequired,
+		video: React.PropTypes.string.idRequired,
 	},
 	render: function(){
+    return (
 		React.createElement('div', {className: 'card_right_button'},
-				React.createElement('a', {href: his.props.movie.video, target: '_blank'}, 'Watch triler' )
+				React.createElement('a', {href: this.props.videoSrc, target: '_blank'}, 'Watch triler' )
+		)
+   )
+	}
+});
+
+// 	CARD LEFT
+		
+var CardLeft = React.createClass({
+	propTypes: {
+		image: React.PropTypes.string.isRequired, 
+	},
+	render: function(){
+		return (
+			React.createElement('div', {className: 'card_left'},
+				React.createElement('img', {src: this.props.image})
+			)
 		)
 	}
 });
 
-var element = React.createElement('section', {},
-		React.createElement('div', {className: 'container'},
-		React.createElement('h1', {}, 'My favorite Films:'), 
-		card
-	)
-);
-		
-var Card = movies.map(function(movie) {
+// CARD RIGHT
+
+var CardRight = React.createClass({
+	propTypes: {
+		movie: React.PropTypes.object.isRequired,
+	},
+	render: function(){
+		return (
+				React.createElement('div', {className: 'card_right'},
+					React.createElement(MovieTitle, { title: this.props.movie.title}),
+					React.createElement(MovieDetails, { movie: this.props.movie}),
+					React.createElement(MovieDescription, { desc: this.props.movie.desc }),
+					React.createElement(MovieButton, { video: this.props.movie.video})
+				)
+		)
+	}
+});
+
+// MOVIE
+
+var Movie = React.createClass({ 
+	propTypes: {
+		movie: React.PropTypes.object.isRequired,
+	},
+	render: function(){
+		return (
+				React.createElement('div', {className: 'card'},	
+					React.createElement(CardLeft, {image: this.props.movie.imgSrc}, ''),
+					React.createElement(CardRight, { movie: this.props.movie }, '')
+				)
+		)
+	}
+});
+
+
+var Cards = movies.map(function(movie) {
         return React.createElement(Movie, {key: movie.id, movie: movie})
 
 });
+
+var element = React.createElement('section', {},
+		React.createElement('div', {className: 'container'}, React.createElement('h1', {}, 'My favorite Films:'), Cards)
+);
 
 ReactDOM.render(element, document.getElementById('app'));
